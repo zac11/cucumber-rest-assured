6b61720f189214478d0356d1267f0d0c719596f9
zac11 <ydv.rahul09@gmail.com>
2019-10-01 22:55:12 +0530

    Added properties file and made changes to pom.xml

---
+++ src/test/java/com/cucum/TestRunner/TestRunner.java
@@ -0,0 +1,25 @@
+package com.cucum.TestRunner;
+import java.io.File;
+
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/java/com/cucum/features",
+plugin= {"com.aventstack.extentreports.offline.commons.css.icons.material"},
+glue= {"com.cucum.stepDefinitions"},
+monochrome=true
+)
+public class TestRunner {
+	public static File temp = new File("resources/temp.xml");
+	
+	@BeforeClass
+	public static void setup() {
+		
+	}
+
+}
